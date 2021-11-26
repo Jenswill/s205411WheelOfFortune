fafa9a38252525be9d8aabab321210d01c52066f
Jens <jenswilliversen@gmail.com>
2021-11-26 17:58:11 +0100

    minor commenting adjustments in gameFragment.kt

--- app/src/main/java/com/wheeloffortune/s205411wheeloffortune/gameFragment.kt
+++ app/src/main/java/com/wheeloffortune/s205411wheeloffortune/gameFragment.kt
@@ -16,6 +16,7 @@ class gameFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // The following code is taken from the following sources and modified:
         // https://stackoverflow.com/questions/56749461/how-to-set-an-onclicklistener-to-a-button-in-kotlin/56749553
         // https://www.youtube.com/watch?v=DI0NIk-7cz8&t=619s&ab_channel=Stevdza-San
         val view : View =  inflater.inflate(R.layout.fragment_game, container, false)
