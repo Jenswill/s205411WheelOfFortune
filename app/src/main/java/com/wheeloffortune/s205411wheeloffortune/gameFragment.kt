29b9dda8f9acd7b1efa0712a88cf7154c05fc691
Jens <jenswilliversen@gmail.com>
2021-11-27 13:27:05 +0100

    made a textview to show which category the word to be guessed is a part of

--- app/src/main/java/com/wheeloffortune/s205411wheeloffortune/gameFragment.kt
+++ app/src/main/java/com/wheeloffortune/s205411wheeloffortune/gameFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.navigation.Navigation
+import kotlin.random.Random
 
 
 class gameFragment : Fragment() {
@@ -20,14 +22,28 @@ class gameFragment : Fragment() {
         // https://stackoverflow.com/questions/56749461/how-to-set-an-onclicklistener-to-a-button-in-kotlin/56749553
         // https://www.youtube.com/watch?v=DI0NIk-7cz8&t=619s&ab_channel=Stevdza-San
         val view : View =  inflater.inflate(R.layout.fragment_game, container, false)
-        val button = view.findViewById<Button>(R.id.buttonTest)
+        val button = view.findViewById<Button>(R.id.giveUpButton)
+        val category = view.findViewById<TextView>(R.id.categoryTextView)
         button.setOnClickListener{ Navigation.findNavController(view).navigate(R.id.navigateToCategories)}
+
         if (currentCategory != null){
-            button.text = currentCategory
+            if (currentCategory == "Random"){
+                val categories = context?.resources?.getStringArray(R.array.Categories)
+                currentCategory = getRandomString(categories as Array<String>)
+
+            }
+
+            category.text = "category: " + currentCategory
+
         }
 
         return view
     }
-
+    // This method is made with code taken from
+    // https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.random/-random/
+    private fun getRandomString(StringArray: Array<String>): String{
+        val randomNumber = Random.nextInt(1,StringArray.size)
+        return StringArray[randomNumber]
+    }
 
 }
